1f383ce3180377b33786196c6fc9529059a66ef4
Jeffrey Shi <jeffreyshi430@gmail.com>
2026-02-25 23:05:01 -0500

    6

--- cogs/summary_cog.py
+++ cogs/summary_cog.py
@@ -8,7 +8,7 @@ from zoneinfo import ZoneInfo
 import asyncio
 
 class SummaryCog(commands.Cog):
-    TARGET_TIME = time(hour=23, minute=2)  # Midnight
+    TARGET_TIME = time(hour=23, minute=6)  # Midnight
     TARGET_ZONE = ZoneInfo("America/New_York")  # EST/EDT automatically
 
     def __init__(self, bot):
